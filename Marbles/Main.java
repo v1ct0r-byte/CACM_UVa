fd8f9b2f81811f7d80781b8fbae78348a3d2f388
Víctor García Valero <garciavicval@gmail.com>
2021-04-14 14:20:25 +0200

    Marbles strategy not accomplished yet

--- Marbles/Main.java
+++ Marbles/Main.java
@@ -1,4 +1,4 @@
-package Marbles;
+//package Marbles;
 import java.util.*;
 
 public class Main {
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         while(scanner.hasNextLine()){
             long n = scanner.nextLong();
+            if(n == 0){break;}
             scanner.nextLine();
             long c1 = scanner.nextLong();
             long n1 = scanner.nextLong();
@@ -15,15 +16,35 @@ public class Main {
             long c2 = scanner.nextLong();
             long n2 = scanner.nextLong();
 
+            long gcdCC = gcd(c1, c2);
+
             //Strategy
             long m1 = 0, m2 = 0;
-            while(c1*n1+c2*n2 != n){
-                if(){
+            while(n1 * x + n2 * y < N){
 
+            }
+            if(c1*x < c2 * y){
+                //Maximize y
+                while((c1 * x + c2 * y) == k * gcdCC){
+                    y++;
+                }
+            }else{
+                //Maximize x
+                while((c1 * x + c2 * y) == k * gcdCC){
+                    x++;
                 }
             }
             
             scanner.nextLine();
         }
+    }
+
+    static long gcd(long a, long b) 
+    { 
+        if (a < b) 
+            return gcd(b, a); 
+        if (a % b == 0) 
+            return b; 
+        return gcd(b, a % b); 
     }
 }
